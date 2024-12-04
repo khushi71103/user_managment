91caad1dac98a7b2603e8449f966ef08b91112be
Khushi Chhetri <khushi71103@gmail.com>
2024-12-04 13:22:42 +0530

    created files under utils for authentication and api handle

---
+++ user-managment/src/utils/api.js
@@ -0,0 +1,17 @@
+
+import axios from 'axios';
+import { getToken } from './auth';
+
+const api = axios.create({
+  baseURL: 'https://reqres.in/api',
+});
+
+api.interceptors.request.use((config) => {
+  const token = getToken();
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
